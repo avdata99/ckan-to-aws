aa779d1c4b12030657b90e085a13a3db4eb071fa
avdata99 <andres@data99.com.ar>
2025-11-05 20:17:29 -0300

    remove already defined settings (in the extension folder)

--- docker/ckan/files/scripts/setup-ckan-ini-file.sh
+++ docker/ckan/files/scripts/setup-ckan-ini-file.sh
@@ -72,17 +72,6 @@ fi
 ckan config-tool ${CKAN_INI} -s logger_ckan "level = INFO"
 ckan config-tool ${CKAN_INI} -s logger_ckanext "level = INFO"
 
-# push-errors-settings
-if [ -z "${SLACK_WEBHOOK_URL}" ]; then
-  echo "SLACK_WEBHOOK_URL is not set. push-errors will not be configured."
-else
-  echo "Configuring push-errors with SLACK_WEBHOOK_URL"
-  ckan config-tool ${CKAN_INI} "ckanext.push_errors.url = ${SLACK_WEBHOOK_URL}"
-  ckan config-tool ${CKAN_INI} "ckanext.push_errors.method = POST"
-  ckan config-tool ${CKAN_INI} "ckanext.push_errors.headers={}"
-  ckan config-tool ${CKAN_INI} "ckanext.push_errors.data={\"text\": \"{message}\", \"username\": \"CKAN AWS LOGS\", \"icon_url\": \"https://github.com/unckan/ckanext-push-errors/raw/main/icons/server-error.png\"}"
-fi
-
 # Run extension-specific ini configuration
 EXTENSIONS_LIST_FILE="${APP_DIR}/extensions/extensions.list.txt"
 if [ -f "$EXTENSIONS_LIST_FILE" ]; then
