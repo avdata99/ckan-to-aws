2163e9a76f0ab559d664c0e3cdc1d67ae98def51
avdata99 <andres@data99.com.ar>
2025-11-06 20:06:32 -0300

    Drop old version

--- terraform/modules/rds/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "db_endpoint" {
-  value = aws_db_instance.main.endpoint
-}
-
-output "db_address" {
-  value = aws_db_instance.main.address
-}
--- terraform/modules/security_groups/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "environment" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
