6b767f5c1f9155eb5ea814c00d6e34a22a728e8c
avdata99 <andres@data99.com.ar>
2025-11-23 18:38:15 -0300

    fix datastore user

--- docker/ckan/files/scripts/setup-datastore.sh
+++ docker/ckan/files/scripts/setup-datastore.sh
@@ -12,10 +12,12 @@ export PGPASSWORD=$DB_PASSWORD
 echo "Creating CKAN database if not exists"
 psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "CREATE DATABASE $DB_NAME;" 2>/dev/null || echo "Database 'ckan' already exists"
 
-echo "Creating datastore read user if not exists"
-psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "CREATE USER $DATASTORE_READ_USERNAME WITH PASSWORD '$DATASTORE_READ_PASSWORD' NOSUPERUSER NOCREATEDB NOCREATEROLE;" || echo "User '$DATASTORE_READ_USERNAME' already exists"
-echo "Creating datastore write user if not exists"
-psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "CREATE USER $DATASTORE_WRITE_USERNAME WITH PASSWORD '$DATASTORE_WRITE_PASSWORD' NOSUPERUSER NOCREATEDB NOCREATEROLE;" || echo "User '$DATASTORE_WRITE_USERNAME' already exists"
+echo "Creating/updating datastore read user"
+psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "DO \$\$ BEGIN CREATE USER $DATASTORE_READ_USERNAME WITH PASSWORD '$DATASTORE_READ_PASSWORD' NOSUPERUSER NOCREATEDB NOCREATEROLE; EXCEPTION WHEN duplicate_object THEN ALTER USER $DATASTORE_READ_USERNAME WITH PASSWORD '$DATASTORE_READ_PASSWORD'; END \$\$;"
+
+echo "Creating/updating datastore write user"
+psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "DO \$\$ BEGIN CREATE USER $DATASTORE_WRITE_USERNAME WITH PASSWORD '$DATASTORE_WRITE_PASSWORD' NOSUPERUSER NOCREATEDB NOCREATEROLE; EXCEPTION WHEN duplicate_object THEN ALTER USER $DATASTORE_WRITE_USERNAME WITH PASSWORD '$DATASTORE_WRITE_PASSWORD'; END \$\$;"
+
 
 echo "Creating datastore db if not exists"
 psql -h $DB_HOST -U $DB_USERNAME -d postgres -c "CREATE DATABASE $DATASTORE_DB_NAME;" 2>/dev/null || echo "Database '$DATASTORE_DB_NAME' already exists"
