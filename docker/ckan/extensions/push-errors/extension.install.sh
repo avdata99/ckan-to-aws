3eb7e010683dd47e06c1d5a38867e67703e06e78
avdata99 <andres@data99.com.ar>
2025-12-09 19:08:20 -0300

    adding push-errors extension

--- docker/ckan/extensions/push-errors/extension.install.sh
+++ docker/ckan/extensions/push-errors/extension.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "Installing Push Errors extension"
 TEMP_DIR=$(mktemp -d)
-git clone --depth 1 --branch 0.1.5 https://github.com/unckan/ckanext-push-errors.git "$TEMP_DIR"
+git clone --depth 1 --branch 0.1.6 https://github.com/unckan/ckanext-push-errors.git "$TEMP_DIR"
 pip install -q "$TEMP_DIR"
 pip install -r "$TEMP_DIR/requirements.txt"
 rm -rf "$TEMP_DIR"
