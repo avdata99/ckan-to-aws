cdbed4ca6c0f11534fbd55773d34bd32b433affa
avdata99 <andres@data99.com.ar>
2025-12-09 22:03:42 -0300

    testing more extensions

--- docker/ckan/extensions/announcements/extension.install.sh
+++ docker/ckan/extensions/announcements/extension.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "Installing Announcements extension"
 TEMP_DIR=$(mktemp -d)
-git clone --depth 1 --branch 0.1.4 https://github.com/okfn/ckanext-announcements.git "$TEMP_DIR"
+git clone --depth 1 --branch 0.1.6 https://github.com/okfn/ckanext-announcements.git "$TEMP_DIR"
 pip install -q "$TEMP_DIR"
 pip install -q -r "$TEMP_DIR/requirements.txt"
 rm -rf "$TEMP_DIR"
--- docker/ckan/extensions/api-tracking/extension.install.sh
+++ docker/ckan/extensions/api-tracking/extension.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 echo "Installing API-tracking extension"
 TEMP_DIR=$(mktemp -d)
-git clone --depth 1 --branch 0.5.2 https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git "$TEMP_DIR"
+git clone --depth 1 --branch 0.5.3 https://github.com/NorwegianRefugeeCouncil/ckanext-api-tracking.git "$TEMP_DIR"
 pip install -q "$TEMP_DIR"
 pip install -q -r "$TEMP_DIR/requirements.txt"
 rm -rf "$TEMP_DIR"
