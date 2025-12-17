3d13eb456a59bd32d66a640b1bb09d87609f137c
avdata99 <andres@data99.com.ar>
2025-12-16 21:47:41 -0300

    fixes for pusher and xloader

--- docker/ckan/extensions/core-datapusher/extension.ini.sh
+++ docker/ckan/extensions/core-datapusher/extension.ini.sh
@@ -1,2 +1,4 @@
 echo "Enabling DataPusher extension settings..."
+ckan config-tool ${CKAN_INI} "ckan.datapusher.url = http://127.0.0.1:8800/"
 ckan config-tool ${CKAN_INI} "ckan.datapusher.enabled = true"
+# ckan config-tool ${CKAN_INI} "ckan.datapusher.api_token = ${DATAPUSHER_TOKEN}"
--- docker/ckan/extensions/xloader/extension.ini.sh
+++ docker/ckan/extensions/xloader/extension.ini.sh
@@ -1,9 +1,9 @@
-echo "Creating a valid API token for Datapusher+"
-DATAPUSHER_TOKEN=$(ckan user token add default xloader_token | tail -n 1 | tr -d '\t')
-echo "Setting xloader configuration in CKAN ini file"
-ckan config-tool ${CKAN_INI} "ckanext.xloader.api_token = ${DATAPUSHER_TOKEN}"
+# echo "Creating a valid API token for XLoader+"
+# DATAPUSHER_TOKEN=$(ckan user token add default xloader_token | tail -n 1 | tr -d '\t')
+# echo "Setting xloader configuration in CKAN ini file"
+# ckan config-tool ${CKAN_INI} "ckanext.xloader.api_token = ${DATAPUSHER_TOKEN}"
 
-echo "Setting for xloader"
-ckan config-tool ${CKAN_INI} "ckanext.xloader.jobs_db.uri = ${SQLALCHEMY_URL}"
+# echo "Setting for xloader"
+# ckan config-tool ${CKAN_INI} "ckanext.xloader.jobs_db.uri = ${SQLALCHEMY_URL}"
 
 ckan config-tool ${CKAN_INI} "ckanext.xloader.max_content_length = 10000000"
