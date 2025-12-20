7bf088c218eb78b58f5306781ebccb6b48cd7601
avdata99 <andres@data99.com.ar>
2025-12-20 20:30:14 -0300

    Use all extension plugins

--- docker/ckan/extensions/geo-view/extension.ini.sh
+++ docker/ckan/extensions/geo-view/extension.ini.sh
@@ -1,4 +1,4 @@
 # Ensure adding geo_view to default views
-# ckan config-tool ${CKAN_INI} "ckan.views.default_views = ... geo_view ..."
+# ckan config-tool ${CKAN_INI} "ckan.views.default_views = ... geo_view geojson_view wmts_view shp_view ..."
 # the best place to do this so far is a place like the file
 # docker/ckan/files/scripts/private/private-entrypoint.sh
